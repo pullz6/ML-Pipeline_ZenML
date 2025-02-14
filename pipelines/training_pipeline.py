15a127446cc1e2833f6144b7a101e74e4d9cfc07
Sara PG <39550635+pullz6@users.noreply.github.com>
2025-02-14 00:17:16 +0000

    Add files via upload

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -4,13 +4,13 @@ from steps.clean_data import clean_df
 from steps.model_train import train_model 
 from steps.evaluation import evaluate_model
 
-@pipeline
+@pipeline(enable_cache=True)
     
 def train_pipeline(data_path:str): 
     df = ingest_df(data_path)
-    clean_df(df)
-    train_model(df)
-    evaluate_model(df)
+    X_train,X_test,y_train, y_test = clean_df(df)
+    model = train_model(X_train,X_test,y_train, y_test)
+    r2,rmse = evaluate_model(model,X_test,y_test)
     
 
     
